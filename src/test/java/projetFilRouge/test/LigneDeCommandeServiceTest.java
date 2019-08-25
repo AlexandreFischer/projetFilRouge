28e6365d8a79cd1089d3853800dc191338384ef8
AlexandreFischer <alexandre.fischer.pro@gmail.com>
2019-08-25 15:41:55 +0200

    Création des fichiers LigneDeCommandeService et LigneDeCommandeServiceTest (vide, sans méthodes ou autres)

---
+++ src/test/java/projetFilRouge/test/LigneDeCommandeServiceTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetFilRouge.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import projetFilRouge.service.LigneDeCommandeService;
+import projetFilRouge.spring.SpringConfig;
+
+/**
+ *
+ * @author alexa
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+@Rollback(false)
+@Transactional
+public class LigneDeCommandeServiceTest {
+    
+    @Autowired
+    private LigneDeCommandeService ligneCmdS;
+    
+    @Test
+    public void ajouterLigneDeCommandeOK(){
+        
+    }
+    
+}
